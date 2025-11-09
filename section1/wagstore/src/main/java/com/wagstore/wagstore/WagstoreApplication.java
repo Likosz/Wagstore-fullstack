335136f73f90e8e17dadee9ee5a43c6767e78802
Wagner <wagnerhudson1@gmail.com>
2025-11-09 18:02:38 -0300

    Initial commit with wagstore project

---
+++ section1/wagstore/src/main/java/com/wagstore/wagstore/WagstoreApplication.java
@@ -0,0 +1,13 @@
+package com.wagstore.wagstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagstoreApplication.class, args);
+	}
+
+}
