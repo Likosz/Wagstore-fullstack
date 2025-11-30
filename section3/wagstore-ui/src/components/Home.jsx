c0e1c35e8c8bc4987b04dea642af5b5892646421
Wagner <wagnerhudson1@gmail.com>
2025-11-30 20:02:02 -0300

    Styling using tailwindcss

--- section3/wagstore-ui/src/components/Home.jsx
+++ section3/wagstore-ui/src/components/Home.jsx
@@ -4,8 +4,8 @@ import ProductListings from "./ProductListings";
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <PageHeading title="Explore Wag Stickers!">
+    <div className="max-w-6xl mx-auto px-6 py-8">
+      <PageHeading title="Explore Eazy Stickers!">
         Adicione um toque de criatividade ao seu espaço com a nossa vasta gama
         de figurinhas divertidas e exclusivas. Perfeitas para qualquer ocasião!
       </PageHeading>
--- section3/wagstore-ui/src/components/footer/Footer.jsx
+++ section3/wagstore-ui/src/components/footer/Footer.jsx
@@ -1,18 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import "./footer.css";
 
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer className="flex justify-center items-center py-4 font-primary text-gray-700">
       Construído com
       <FontAwesomeIcon
         icon={faHeart}
-        className="footer-icon"
+        className="text-red-600 mx-1 animate-pulse"
         aria-hidden="true"
       />
       by
-      <a href="https://github.com/Likosz" target="_blank" rel="noreferrer">
+      <a
+        href="https://github.com/Likosz"
+        target="_blank"
+        rel="noreferrer"
+        className="text-primary font-semibold px-1 transition-colors duration-300 hover:text-dark"
+      >
         Wagner(Likosz)
       </a>
     </footer>
